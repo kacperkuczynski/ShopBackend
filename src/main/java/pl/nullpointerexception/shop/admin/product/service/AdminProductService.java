30d3205830a24466caf028bfc109e685f2dfb5f5
kacperkuczynski <kacper.kuczynski09@gmail.com>
2022-11-23 10:14:51 +0100

    dodawanie i edycja produktu

--- src/main/java/pl/nullpointerexception/shop/admin/product/service/AdminProductService.java
+++ src/main/java/pl/nullpointerexception/shop/admin/product/service/AdminProductService.java
@@ -12,7 +12,20 @@ import pl.nullpointerexception.shop.admin.product.repository.AdminProductReposit
 public class AdminProductService {
 
     private final AdminProductRepository productRepository;
+
     public Page<AdminProduct> getProducts(Pageable pageable){
         return productRepository.findAll(pageable);
+    }
+
+    public AdminProduct getProduct(Long id) {
+        return productRepository.findById(id).orElseThrow();
+    }
+
+    public AdminProduct createProduct(AdminProduct product) {
+        return productRepository.save(product);
+    }
+
+    public AdminProduct updateProduct(AdminProduct product) {
+        return productRepository.save(product);
     }
 }
