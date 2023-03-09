008b4e220b77319301ef342c2d0b5df3a1c3b505
kacperkuczynski <59393105+kacperkuczynski@users.noreply.github.com>
2023-03-09 11:06:56 +0100

    Przetwarzanie zamówień – zmiana statusu

--- src/main/java/pl/shop/admin/order/model/AdminOrderStatus.java
+++ src/main/java/pl/shop/admin/order/model/AdminOrderStatus.java
@@ -1,5 +1,20 @@
 package pl.shop.admin.order.model;
 
 public enum AdminOrderStatus {
-    NEW
+    NEW("Nowe"),
+    PAID("Opłacone"),
+    PROCESSING("Przetwarzane"),
+    WAITING_FOR_DELIVERY("Czaeka na dostawę"),
+    COMPLETED("Zrealizowane"),
+    CANCELED("Anulowane"),
+    REFUND("Zwrócone");
+
+    private String value;
+    AdminOrderStatus(String value){
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
