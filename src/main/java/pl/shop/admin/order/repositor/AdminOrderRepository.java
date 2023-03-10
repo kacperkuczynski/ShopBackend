7c781b15477492798f725d65e7fa96983b6110b3
kacperkuczynski <59393105+kacperkuczynski@users.noreply.github.com>
2023-03-10 17:27:35 +0100

    Eksport zamówień

--- src/main/java/pl/shop/admin/order/repositor/AdminOrderRepository.java
+++ src/main/java/pl/shop/admin/order/repositor/AdminOrderRepository.java
@@ -2,7 +2,12 @@ package pl.shop.admin.order.repositor;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.shop.admin.order.model.AdminOrder;
+import pl.shop.admin.order.model.AdminOrderStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 
 public interface AdminOrderRepository extends JpaRepository<AdminOrder, Long> {
+    List<AdminOrder> findAllByPlaceDateIsBetweenAndOrderStatus(LocalDateTime from, LocalDateTime to, AdminOrderStatus orderStatus);
 }
---
+++ src/main/java/pl/shop/admin/order/service/AdminExportService.java
@@ -0,0 +1,20 @@
+package pl.shop.admin.order.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.shop.admin.order.model.AdminOrder;
+import pl.shop.admin.order.model.AdminOrderStatus;
+import pl.shop.admin.order.repositor.AdminOrderRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AdminExportService {
+
+    private final AdminOrderRepository orderRepository;
+    public List<AdminOrder> exportOrders(LocalDateTime from, LocalDateTime to, AdminOrderStatus orderStatus) {
+        return orderRepository.findAllByPlaceDateIsBetweenAndOrderStatus(from, to, orderStatus);
+    }
+}
